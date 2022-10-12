f429cbcfaf76fbbf0894b8148ac06555f396d33a
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-10-11 22:08:54 -0400

    updated server.js

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ const PORT = process.env.PORT || 3001;
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
 app.use(routes);
 
+mongoose.connect(process.env.MONGODB_URI || 'mongob://localhost/social-network', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 
+}); 
 
+mongodb.set('debug', true);
+
+app.listen(PORT, () => console.log(`listening on localhost:${PORT}`));
