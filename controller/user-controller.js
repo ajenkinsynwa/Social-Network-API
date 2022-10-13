b27f754264c20c7c4365655f30b86e5da2676c5b
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-10-12 23:48:12 -0400

    updated user-controller.js

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -42,5 +42,35 @@ updateUser({ params, body}, res) {
 
   },
 
-  
+  deleteUser ({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({message: 'No User Found With This Id'});
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err =>status(400).json(err))
+  },
+
+  getUserbyId({params}, res) {
+    User.findOne({ _id: params.id})
+    .populate({
+      path: 'thoughts',
+      select: '-__v'
+    })
+    .populate ({
+      path: 'friends',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    });
+  }
+
+  module.exports = userController
   
