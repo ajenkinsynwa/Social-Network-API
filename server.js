4ebf7f658b0522d3d38c57f48b32f556c2072e01
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-10-14 15:24:40 -0400

    removed usefindandmodify setting

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network', {
-  useFindAndModify: false,
+ 
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
