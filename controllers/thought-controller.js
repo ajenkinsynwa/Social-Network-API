2ae4cc4c371020c9623bc5aa16f37f03397a3132
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-10-16 19:55:07 -0400

    updated thoughts controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -3,12 +3,12 @@ const { Thought, User } = require('../models')
 //Get All Thoughts
 const thoughtController = {
   getAllThoughts(req, res) {
-    Thought.find({})
-    .populate({
-      path: 'user',
-      select: '__v'
-    })
-    .select('__v')
+    Thought.find()
+    // .populate({
+    //   path: 'user',
+    //   select: '__v'
+    // })
+    // .select('__v')
     .sort({_id: -1})
     .then(dbThoughtData => res.json(dbThoughtData))
     .catch(err => {
@@ -20,11 +20,11 @@ const thoughtController = {
 //Get Thought By ID
 getThoughtById ({ params }, res) {
   Thought.findOne({ _id: params.id })
-  .populate({
-    path: 'user',
-    select: '-__v'
-  })
-  .select('__v')
+  // .populate({
+  //   path: 'user',
+  //   select: '-__v'
+  // })
+  // .select('__v')
   .sort({_id: -1})
   .then(dbThoughtData => res.json(dbThoughtData))
   .catch(err => {
